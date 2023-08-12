c0fc49d3170be96126c6e6e16c7c7785cebf5530
SampadaKayat <126313520+SampadaKayat@users.noreply.github.com>
2023-08-12 12:24:37 +0530

    Initial commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,46 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  {
+    id: 1,
+    avatar:
+      'https://m.media-amazon.com/images/I/71fnKRNIFEL._AC_UF1000,1000_QL80_.jpg',
+    name: 'Lata Mangeshkar',
+    field: 'Music',
+    info: 'Lata was born on September 28, 1929, to classical singer and theatre artist Pandit Deenanath Mangeshkar and Shevanti in Indore, Madhya Pradesh. Her father started teaching her music at an early age. By the time she was five, Lata was seen participating as an actress in plays written by her father.',
+  },
+
+  {
+    id: 2,
+    avatar: 'https://upload.wikimedia.org/wikipedia/commons/1/10/RatanTata.jpg',
+    name: 'Ratan Tata',
+    field: 'Industry ',
+    info: 'Ratan Tata was the interim chairman of Tata Sons. He continues to head the main two Tata trusts Sir Dorabji Tata and Allied Trusts and Sir Ratan Tata Trust and their allied trusts, with a combined stake of 66% in Tata Sons, Tata group',
+  },
+
+  {
+    id: 3,
+    avatar: 'https://cdn.siasat.com/wp-content/uploads/2022/04/Allu-arjun.jpg',
+    name: 'Allu Arjun ',
+    field: 'Indian actor',
+    info: 'Allu Arjun was born on 8 April 1982 in a Telugu family in Madras (present-day Chennai) to film producer Allu Aravind and Nirmala. His paternal grandfather was the noted film comedian Allu Ramalingaiah who appeared in over 1000 films. Their native place is Palakollu of West Godavari district, Andhra Pradesh.',
+  },
+
+  {
+    id: 4,
+    avatar:
+      'https://m.media-amazon.com/images/M/MV5BZjVlNWQ4YmItOGVkYi00MzM4LWFkZTUtYjA5OTVkNTNkMWRiL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_FMjpg_UX1000_.jpg',
+    name: 'Virat Kohli',
+    field: 'Indian cricketer',
+    info: 'Born and raised in New Delhi, Kohli trained at the West Delhi Cricket Academy and started his youth career with the Delhi Under-15 team. He made his international debut in 2008 and quickly became a key player in the ODI team and later made his Test debut in 2011.',
+  },
+
+  {
+    id: 5,
+    avatar:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Srushti_Deshmukh_Gowda.jpg/1200px-Srushti_Deshmukh_Gowda.jpg',
+    name: 'Srushti Deshmukh',
+    field: 'Civil service officer',
+    info: 'Srishti Deshmukh, who became an IAS in her 1st UPSC attempt, was born on 28 March 1995 in Kasturba Nagar, Bhopal, MP, and is mostly popular for securing fifth rank in the UPSC exam 2018. Aside from this, videos of her duty usually go viral on YouTube and Instagram reels, due to which She has got more popularity.',
+  },
+];
